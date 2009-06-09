8e61d73cc6c94e944cbeb19fab7ab01e4120a6af
josejamilena <josejamilena@c2c3169c-52ad-11de-9eea-55519c9c5eb7>
2009-06-09 17:46:33 +0000

    git-svn-id: https://pfc-jose.googlecode.com/svn/trunk@15 c2c3169c-52ad-11de-9eea-55519c9c5eb7

---
+++ 1 - analisis de requisitos/prospecciones java/practicalb/src/GUI/GestorVentanas.java
@@ -0,0 +1,150 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+/**
+ * Clase encarga de gestionar las distintas ventanas de la aplicaci�n
+ * 
+ * @author Jose Jamilena Daza, Miguel A. Moreno Leiva y Pablo Land�n Romero
+ */
+public class GestorVentanas extends JFrame {
+
+    private JFrame ventSeleccion = null;
+    private JFrame ventMuestra = null;
+    //private JFrame ventBusqueda = null;
+    private JFrame ventDatosConex = null;
+    private JFrame ventSalida = null;  
+    private Salida s;
+
+    /**
+     * Constructor
+     */
+    public GestorVentanas(){       
+    }
+
+    public void crearVentanaDatosConex(){
+        DatosConexion a = new DatosConexion(this);
+        ventDatosConex = new JFrame("Identificacion");
+        getVentanaDatosConex().setContentPane(a);
+        getVentanaDatosConex().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getVentanaDatosConex().pack();
+        getVentanaDatosConex().setLocation(250, 100);
+        getVentanaDatosConex().setVisible(true);
+    }
+    
+  
+    /*public void crearVentanaBusqueda() {
+        Busqueda b = new Busqueda(this);
+        ventBusqueda = new JFrame("Busqueda de fichero");
+        getVentanaBusqueda().setContentPane(b);
+        getVentanaBusqueda().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getVentanaBusqueda().pack();
+        getVentanaBusqueda().setLocation(250, 100);
+        getVentanaBusqueda().setVisible(true);
+    }*/
+
+     private void crearVentanaSalida(conex.Comun comun) {
+        s = new Salida(this, comun);
+        ventSalida  = new JFrame("Salida de resultados");
+        getVentanaSalida().setContentPane(s);
+        getVentanaSalida().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getVentanaSalida().setSize(500, 700);
+        getVentanaSalida().setLocation(250, 60);
+        getVentanaSalida().setVisible(true);
+    }
+
+    /**
+     * Crea la ventana de selecci�n
+     */
+    public void crearVentanaSeleccion() {
+        Seleccion s = new Seleccion(this);
+        ventSeleccion = new JFrame("Test");
+        getVentanaSeleccion().setContentPane(s);
+        getVentanaSeleccion().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getVentanaSeleccion().setSize(600, 400);
+        getVentanaSeleccion().setLocation(250, 100);
+        getVentanaSeleccion().setVisible(true);
+    }
+
+
+    
+    public void pasarADatosConex(){
+        if (getVentanaDatosConex() == null) {
+            crearVentanaDatosConex();
+        } else {
+            getVentanaDatosConex().setVisible(true);
+        }
+    }
+
+
+   
+
+
+
+ 
+    
+    /**
+     * Obtiene la ventana principal
+     * @return instancia de la ventana
+     
+    public JFrame getVentanaBusqueda() {
+        return ventBusqueda;
+    }*/
+
+    /**
+     * Obtiene la ventana de selecci�n
+     * @return instancia de la ventana
+     */
+    public JFrame getVentanaSeleccion() {
+        return ventSeleccion;
+    }
+
+    public JFrame getVentanaDatosConex() {
+        return ventDatosConex;
+    }
+
+
+    /**
+     * Obtiene la ventana de muestra
+     * @return instancia de la ventana
+     */
+    public JFrame getVentanaMuestra() {
+        return ventMuestra;
+    }
+
+   /* void pasarABusqueda() {
+        if (getVentanaBusqueda() == null) {
+            crearVentanaBusqueda();
+        } else {
+            getVentanaBusqueda().setVisible(true);
+        }
+    }*/
+
+    void pasarASalida(conex.Comun comun) {
+        getVentanaSeleccion().setVisible(false);
+        if (getVentanaSalida() == null) {
+            crearVentanaSalida(comun);
+        } else {
+            s.cargarDatos();
+            getVentanaSalida().setVisible(true);
+        }
+    }
+
+    void pasarASeleccion() {
+        getVentanaDatosConex().setVisible(false);
+        if (getVentanaSalida() != null) {
+            getVentanaSalida().setVisible(false);
+        }
+        if (getVentanaSeleccion() == null) {
+            crearVentanaSeleccion();
+        } else {
+            getVentanaSeleccion().setVisible(true);
+        }
+    }
+
+   
+    private JFrame getVentanaSalida() {
+        return ventSalida;
+    }
+
+}
---
+++ 1 - analisis de requisitos/prospecciones java/practicalb/src/conex/Principal.java
@@ -0,0 +1,72 @@
+package conex;
+
+
+import java.io.FileNotFoundException;
+
+/**
+ * Clase principal.
+ * @author Jose Antonio Jamilena Daza, Miguel Angel Moreno Leiva, Pablo Landin.
+ */
+public class Principal {
+    /**
+     * main.
+     * @param args argumentos.
+     */
+    public static void main(String[] args) {
+        // mostrarAyuda();
+        
+            // try {            
+            //            String nombreFichero;
+            //            Comun comun = new Comun();
+            //            Conexion conexion = new Conexion();
+            //            Estadisticas estadisticas = new Estadisticas();
+            //            if (args.length == 0) {
+            //                nombreFichero = "ejemplo.sql";
+            //            } else {
+            //                nombreFichero = args[0];
+            //            }
+            //            LanzarConsultasDeFichero lcf = new LanzarConsultasDeFichero(conexion, estadisticas, nombreFichero);
+            //            System.out.println("Media de tiempos del fichero:");
+            //            System.out.println(lcf.mediaDeTiempos());
+            //            System.out.println("Media de tiempos en manipulacion de datos (INSERT / UPDATE /DELETE):");
+            //            System.out.println(estadisticas.mostrarMediaManipulacionDatos());
+            //            System.out.println("Media de tiempos en manipulacion de tablas (DROP / CREATE):");
+            //            System.out.println(estadisticas.mostrarMediaManipulacionTablas());
+            //            System.out.println("Media de tiempos en consultas de selecion (SELECT):");
+            //            System.out.println(estadisticas.mostrarMediaSeleccion());
+            //            System.out.println("Media de tiempos de otras operaciones (?):");
+            //            System.out.println(estadisticas.mostrarMediaOtrasOperaciones());        
+            //            System.out.println("Media de tiempos total:");
+            //            System.out.println(estadisticas.mostrarMedia());            
+            
+            //        } catch (java.sql.SQLException ex) {
+            //            System.err.println("FALLO EN CONEXION CON LA BASE DE DATOS");
+            //        } catch (FileNotFoundException ex) {
+            //            ex.printStackTrace();
+            //        } catch (Exception ex) {
+            //            ex.printStackTrace();
+            //        }
+
+            GUI.GestorVentanas gv = new GUI.GestorVentanas();
+            gv.crearVentanaDatosConex();
+    }
+    //    /**
+    //     * muestra mensaje de ayuda.
+    //     */
+    //    private static void mostrarAyuda() {
+    //        System.out.println();
+    //        System.out.println("=================");
+    //        System.out.println("Benchmark Ligeros");
+    //        System.out.println("=================");
+    //        System.out.println();
+    //        System.out.println("Modo de empleo:");
+    //        System.out.println("java -jar practica.jar ");
+    //        System.out.println("\tAsi, carga el script SQL del fichero ejemplo.sql");
+    //        System.out.println("java -jar practica.jar <ficheroSQL> ");
+    //        System.out.println("\tAsi, carga el script SQL del fichero indicado");
+    //        System.out.println("\tCada linea del fichero ha de ser una sentencia SQL, sino no va");        
+    //        System.out.println();        
+    //        System.out.println("Las conexiones y usuarios se modifican en el fichero config.properties");                
+    //        System.out.println();        
+    //    }
+}
