e4790163345c3a622fb9da7f194ef2270010ed1c
josejamilena <josejamilena@c2c3169c-52ad-11de-9eea-55519c9c5eb7>
2009-06-28 09:28:51 +0000

    git-svn-id: https://pfc-jose.googlecode.com/svn/trunk@88 c2c3169c-52ad-11de-9eea-55519c9c5eb7

--- 4 - implementacion, pruebas y documentacion/4.1 - implementacion/josejamilena.pfc.servidor.chartserver/src/josejamilena/pfc/servidor/chartserver/Webserver.java
+++ 4 - implementacion, pruebas y documentacion/4.1 - implementacion/josejamilena.pfc.servidor.chartserver/src/josejamilena/pfc/servidor/chartserver/Webserver.java
@@ -1,8 +1,12 @@
 package josejamilena.pfc.servidor.chartserver;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 /**
  * Servidor web para mostras gráficas con Google Charts.
@@ -10,11 +14,14 @@ import java.net.Socket;
  */
 public final class Webserver {
 
+    /** properties de configuracion. */
+    private static Properties configProperties;
+
+    /** Logger. */
+    private static Logger logger = Logger.getLogger(Webserver.class);
+
     /** Socket servidor. */
     private static ServerSocket serverSocket;
-
-    /** Puerto HTTP por defecto, el 80. */
-    private static final int DEFAULT_HTTP_PORT = 80;
 
     /** Puerto HTTP empleado. */
     private static int httpPort;
@@ -27,21 +34,22 @@ public final class Webserver {
      * Método principal.
      * @param args puerto HTTP que abrirá, si se indica, usa el 80.
      * @throws java.io.IOException Error.
+     * @throws ClassNotFoundException no encuentra el driver JDBC
      */
-    public static void main(final String[] args) throws IOException {
-        if (args.length < 1) {
-            // Por defecto, inicia en el puerto 80
-            httpPort = DEFAULT_HTTP_PORT;
-            serverSocket = new ServerSocket(httpPort);
-        } else {
-            // Se inicia en en puerto pasado por argumento
-            try {
-                httpPort = Integer.parseInt(args[0]);
-                serverSocket = new ServerSocket(httpPort);
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            }
-        }
+    public static void main(final String[] args)
+            throws IOException, ClassNotFoundException {
+        PropertyConfigurator.configure("log4j.properties");
+        Properties prop = new Properties();
+        prop.load(new FileInputStream("config.properties"));
+        Webserver.setConfigProperties(prop);
+        httpPort = Integer.parseInt(getConfigProperties().getProperty(
+                "josejamilena.pfc.servidor.chartserver.TCPPort"));
+        serverSocket = new ServerSocket(httpPort);
+        logger.info("josejamilena.pfc.servidor.chartserver");
+        logger.info("Iniciado en puerto: " + httpPort);
+        Class.forName(Webserver.getConfigProperties().getProperty(
+                    "josejamilena.pfc.servidor.chartserver.driver")
+                    );
         while (true) {
             try {
                 Socket s = serverSocket.accept();
@@ -51,5 +59,20 @@ public final class Webserver {
             }
         }
     }
+
+    /**
+     * @return the configProperties
+     */
+    public static Properties getConfigProperties() {
+        return configProperties;
+    }
+
+    /**
+     * @param aConfigProperties the configProperties to set
+     */
+    public static void setConfigProperties(final Properties aConfigProperties) {
+        configProperties = aConfigProperties;
+    }
+
 }
 
