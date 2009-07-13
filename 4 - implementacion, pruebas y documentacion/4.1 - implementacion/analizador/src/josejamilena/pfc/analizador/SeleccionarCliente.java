ea5f4a2344ba7ae8ac16189bf468d08a55485fc0
josejamilena <josejamilena@c2c3169c-52ad-11de-9eea-55519c9c5eb7>
2009-07-13 21:25:29 +0000

    no se si va
    git-svn-id: https://pfc-jose.googlecode.com/svn/trunk@115 c2c3169c-52ad-11de-9eea-55519c9c5eb7

--- 4 - implementacion, pruebas y documentacion/4.1 - implementacion/analizador/src/josejamilena/pfc/analizador/SeleccionarCliente.java
+++ 4 - implementacion, pruebas y documentacion/4.1 - implementacion/analizador/src/josejamilena/pfc/analizador/SeleccionarCliente.java
@@ -2,9 +2,9 @@ package josejamilena.pfc.analizador;
 
 import java.sql.SQLException;
 import java.util.List;
-import javax.swing.WindowConstants;
+import javax.swing.JFrame;
+import josejamilena.pfc.analizador.sql.SQLUtils;
 import org.jdesktop.application.Action;
-import org.jfree.ui.RefineryUtilities;
 
 public class SeleccionarCliente extends javax.swing.JDialog {
 
@@ -15,40 +15,48 @@ public class SeleccionarCliente extends javax.swing.JDialog {
         this.setTitle("Seleccione...");
         lista = lst;
         initComponents();
-        getRootPane().setDefaultButton(closeButton);
+        getRootPane().setDefaultButton(siguienteButton);
     }
 
     @Action
-    public void closeSelecionarScriptBox() {
-        dispose();
+    public void siguienteSelecionarScriptBox() {
+        try {
+            JFrame mainFrame = App.getApplication().getMainFrame();
+            SeleccionarClienteScript scs = new SeleccionarClienteScript(mainFrame, SQLUtils.listaScript(App.conn), (String) listaScript.getSelectedItem());
+            scs.setLocationRelativeTo(mainFrame);
+            scs.setSize(300, 600);
+            scs.pack();
+            scs.setResizable(false);
+            App.getApplication().show(scs);
+        } catch (SQLException ex) {
+            new MsgBox("Mensaje de Error", ex.getSQLState(), "ERROR");
+        } finally {
+            dispose();
+        }
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        closeButton = new javax.swing.JButton();
+        siguienteButton = new javax.swing.JButton();
         listaScript = new javax.swing.JComboBox();
-        cargarGrafica = new javax.swing.JButton();
         icono = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setName("Form"); // NOI18N
 
         javax.swing.ActionMap actionMap = org.jdesktop.application.Application.getInstance(josejamilena.pfc.analizador.App.class).getContext().getActionMap(SeleccionarCliente.class, this);
-        closeButton.setAction(actionMap.get("closeSelecionarScriptBox")); // NOI18N
+        siguienteButton.setAction(actionMap.get("siguienteSelecionarScriptBox")); // NOI18N
         org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(josejamilena.pfc.analizador.App.class).getContext().getResourceMap(SeleccionarCliente.class);
-        closeButton.setText(resourceMap.getString("closeButton.text")); // NOI18N
-        closeButton.setName("closeButton"); // NOI18N
+        siguienteButton.setText(resourceMap.getString("siguienteButton.text")); // NOI18N
+        siguienteButton.setActionCommand(resourceMap.getString("siguienteButton.actionCommand")); // NOI18N
+        siguienteButton.setName("siguienteButton"); // NOI18N
 
         listaScript.setName("listaScript"); // NOI18N
         for (String i : lista) {
             this.listaScript.addItem(i);
         }
-
-        cargarGrafica.setAction(actionMap.get("cargarGrafica")); // NOI18N
-        cargarGrafica.setText(resourceMap.getString("cargarGrafica.text")); // NOI18N
-        cargarGrafica.setName("cargarGrafica"); // NOI18N
 
         icono.setIcon(resourceMap.getIcon("icono.icon")); // NOI18N
         icono.setText(resourceMap.getString("icono.text")); // NOI18N
@@ -62,50 +70,28 @@ public class SeleccionarCliente extends javax.swing.JDialog {
                 .addComponent(icono)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                        .addComponent(listaScript, 0, 184, Short.MAX_VALUE)
-                        .addComponent(cargarGrafica, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(closeButton))
+                    .addComponent(listaScript, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(siguienteButton))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(icono)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(70, 70, 70)
                         .addComponent(listaScript, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(cargarGrafica)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(closeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 143, Short.MAX_VALUE)
+                        .addComponent(siguienteButton, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    @Action
-    public void cargarGrafica() {
-        try {
-            GraficoPorCliente demo = new GraficoPorCliente((String) listaScript.getSelectedItem());
-            demo.pack();
-            RefineryUtilities.centerFrameOnScreen(demo);
-            demo.setVisible(true);
-            demo.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        } finally {
-            dispose();
-        }
-    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton cargarGrafica;
-    private javax.swing.JButton closeButton;
     private javax.swing.JLabel icono;
     private javax.swing.JComboBox listaScript;
+    private javax.swing.JButton siguienteButton;
     // End of variables declaration//GEN-END:variables
 }
