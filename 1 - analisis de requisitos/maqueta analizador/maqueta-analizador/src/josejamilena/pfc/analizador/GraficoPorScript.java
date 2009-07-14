947d32c63e36166c687f228598b818d5c8d774e8
josejamilena <josejamilena@c2c3169c-52ad-11de-9eea-55519c9c5eb7>
2009-07-14 12:10:05 +0000

    git-svn-id: https://pfc-jose.googlecode.com/svn/trunk@119 c2c3169c-52ad-11de-9eea-55519c9c5eb7

---
+++ 1 - analisis de requisitos/maqueta analizador/maqueta-analizador/src/josejamilena/pfc/analizador/GraficoPorScript.java
@@ -0,0 +1,71 @@
+package josejamilena.pfc.analizador;
+
+import java.awt.Color;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class GraficoPorScript extends JDialog {
+
+    public GraficoPorScript(final String script, final String hostSgbd) throws ClassNotFoundException, SQLException {
+
+        Map<String, String> res = new TreeMap<String, String>();
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        String consulta = "select tiempo, fecha from estadisticas where tipo=\'" + script + "\' and host_sgbd=\'" + hostSgbd + "\'";
+        res.put("14:00","12312");
+        res.put("14:05", "3312");
+        res.put("14:10", "1812");
+        res.put("14:15", "1231");
+        res.put("14:20", "2666");
+        res.put("14:25", "1131");
+        res.put("14:30", "9131");
+        res.put("14:35", "3131");
+        res.put("14:40", "6131");
+        res.put("14:45", "1231");
+        res.put("14:50", "5631");
+        res.put("14:55", "1131");
+        res.put("15:00", "2781");
+        res.put("15:05", "2131");
+
+        Iterator it = res.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pairs = (Map.Entry) it.next();
+            dataset.setValue(Double.parseDouble(pairs.getValue().toString()),
+                    script, pairs.getKey().toString());
+        }
+
+
+        JFreeChart chart = ChartFactory.createBarChart(
+                script, // chart title
+                "Hora", // domain axis label
+                "Duración (milisegundos)", // range axis label
+                dataset, // data
+                PlotOrientation.HORIZONTAL,
+                false, // include legend
+                true,
+                false);
+
+        CategoryPlot plot = chart.getCategoryPlot();
+        chart.setBackgroundPaint(Color.white);
+        plot.setOutlinePaint(Color.black);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.getViewport().add((new JPanel()).add(chartPanel));
+        setContentPane(scrollPane);
+    }
+}
+
+
---
+++ 1 - analisis de requisitos/maqueta analizador/maqueta-analizador/src/josejamilena/pfc/analizador/SeleccionarSGBDScript.java
@@ -0,0 +1,113 @@
+package josejamilena.pfc.analizador;
+
+import java.sql.SQLException;
+import java.util.List;
+import javax.swing.WindowConstants;
+import org.jdesktop.application.Action;
+import org.jfree.ui.RefineryUtilities;
+
+public class SeleccionarSGBDScript extends javax.swing.JDialog {
+
+    private List<String> lista = null;
+    private String hostSGBD = "";
+
+    public SeleccionarSGBDScript(java.awt.Frame parent, List<String> lst, String host) {
+        super(parent);
+        this.setTitle("Seleccione...");
+        lista = lst;
+        hostSGBD = host;
+        initComponents();
+        getRootPane().setDefaultButton(closeButton);
+    }
+
+    @Action
+    public void closeSelecionarScriptBox() {
+        dispose();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        closeButton = new javax.swing.JButton();
+        listaScript = new javax.swing.JComboBox();
+        cargarGrafica = new javax.swing.JButton();
+        icono = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setName("Form"); // NOI18N
+
+        javax.swing.ActionMap actionMap = org.jdesktop.application.Application.getInstance(josejamilena.pfc.analizador.App.class).getContext().getActionMap(SeleccionarSGBDScript.class, this);
+        closeButton.setAction(actionMap.get("closeSelecionarScriptBox")); // NOI18N
+        org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(josejamilena.pfc.analizador.App.class).getContext().getResourceMap(SeleccionarSGBDScript.class);
+        closeButton.setText(resourceMap.getString("closeButton.text")); // NOI18N
+        closeButton.setName("closeButton"); // NOI18N
+
+        listaScript.setName("listaScript"); // NOI18N
+        for (String i : lista) {
+            this.listaScript.addItem(i);
+        }
+
+        cargarGrafica.setAction(actionMap.get("cargarGrafica")); // NOI18N
+        cargarGrafica.setText(resourceMap.getString("cargarGrafica.text")); // NOI18N
+        cargarGrafica.setName("cargarGrafica"); // NOI18N
+
+        icono.setIcon(resourceMap.getIcon("icono.icon")); // NOI18N
+        icono.setText(resourceMap.getString("icono.text")); // NOI18N
+        icono.setName("icono"); // NOI18N
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(icono)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(listaScript, 0, 186, Short.MAX_VALUE)
+                        .addComponent(cargarGrafica, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(closeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(icono)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(67, 67, 67)
+                        .addComponent(listaScript, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(cargarGrafica)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(closeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    @Action
+    public void cargarGrafica() {
+        try {
+            GraficoPorSGBD demo = new GraficoPorSGBD(hostSGBD, (String) listaScript.getSelectedItem());
+            demo.pack();
+            RefineryUtilities.centerFrameOnScreen(demo);
+            demo.setVisible(true);
+            demo.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            dispose();
+        }
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cargarGrafica;
+    private javax.swing.JButton closeButton;
+    private javax.swing.JLabel icono;
+    private javax.swing.JComboBox listaScript;
+    // End of variables declaration//GEN-END:variables
+}
