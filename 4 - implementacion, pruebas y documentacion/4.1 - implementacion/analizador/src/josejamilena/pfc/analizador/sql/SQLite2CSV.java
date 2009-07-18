fd95d629d38c93729525473e1f93804aff67c134
josejamilena <josejamilena@c2c3169c-52ad-11de-9eea-55519c9c5eb7>
2009-07-18 00:28:09 +0000

    git-svn-id: https://pfc-jose.googlecode.com/svn/trunk@146 c2c3169c-52ad-11de-9eea-55519c9c5eb7

--- 4 - implementacion, pruebas y documentacion/4.1 - implementacion/analizador/src/josejamilena/pfc/analizador/sql/SQLite2CSV.java
+++ 4 - implementacion, pruebas y documentacion/4.1 - implementacion/analizador/src/josejamilena/pfc/analizador/sql/SQLite2CSV.java
@@ -18,7 +18,6 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
